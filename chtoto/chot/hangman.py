cd543b49893c528b0a95265a7607025e3d74149f
pivnoybochok <166494345+RandomizedBruh@users.noreply.github.com>
2025-03-25 17:27:59 +1000

    Add files via upload

--- chtoto/chot/hangman.py
+++ chtoto/chot/hangman.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
-WORDS = ["минекарп","ФеНеФе","эпигандо"]
+from tkinter import messagebox
+WORDS = ["минекарп","фенефе","эпигандо","опаньки","солевоймарм"]
 
 # 1) выбираем случайное слово
 # 2) создаем окошко, настраиваем его
@@ -22,12 +23,12 @@ root.geometry("500x500")
 canvas = tk.Canvas(root, width=400, height=400,background="yellow")
 canvas.pack()
 
-canvas.create_oval(150,90, 250,180,width=4,fill="green")
-canvas.create_line(200,100, 200,250,width=4,fill="green")
-canvas.create_line(140,250, 200,200,width=4,fill="green")
-canvas.create_line(200,200, 260,250,width=4,fill="green")
-canvas.create_line(160,300, 200,250,width=4,fill="green")
-canvas.create_line(230,360, 80,50,width=4,fill="green")
+# canvas.create_oval(150,90, 250,180,width=4,fill="green")
+# canvas.create_line(200,100, 200,250,width=4,fill="green")
+# canvas.create_line(140,250, 200,200,width=4,fill="green")
+# canvas.create_line(200,200, 260,250,width=4,fill="green")
+# canvas.create_line(160,300, 200,250,width=4,fill="green")
+# canvas.create_line(230,300, 200,250,width=4,fill="green")
 
 
 
@@ -38,17 +39,76 @@ canvas.create_line(200,20, 200,100, width=10, fill="black")
 
 
 def draw_hangman(attempt):
+
     # головав
-    if attempt == 1:  
+    if attempt == 5:  
         canvas.create_oval(150,90, 250,180,width=4,fill="green")
         # Тэло
-    elif attempt == 2:  
+    elif attempt == 4:  
         canvas.create_line(200,100, 200,250,width=4,fill="green")
-draw_hangman(2)
+    elif attempt == 3:  
+        canvas.create_line(140,250, 200,200,width=4,fill="green")
+    elif attempt == 2:  
+        canvas.create_line(200,200, 260,250,width=4,fill="green")
+    elif attempt == 1:  
+        canvas.create_line(160,300, 200,250,width=4,fill="green")
+    elif attempt == 0:  
+        canvas.create_line(230,300, 200,250,width=4,fill="green")
+
+def validate_count_symb(e):
+    s=entry.get().strip()
+    print(s)
+    if len(s) >1:
+     messagebox.showinfo("ничего себе","куда тебе столько букв, балбес")
+    s=s[-1] if s in range(0,1) else ''
+    entry.delete('1 ',tk.END)
+    entry.insert(tk.INSERT,s)
+    print(e.char)
+
+
+def display_word():
+    displayed_word=""
+    for letter in word:
+        if letter in guessed_letters:
+            displayed_word+= " "
+        else:
+            displayed_word+=" _"
+    return displayed_word.strip()
+
+
+def check_letter():
+    global attempts
+    letter=entry.get().lower()
+    entry.delete(0,tk.END)
+    if letter in guessed_letters:
+        messagebox.showinfo("Повтр","опа, а буква уже есть")
+        return
+    guessed_letters.append(letter)
+
+    if letter not in word:
+        attempts-=1
+        draw_hangman(attempts)
+    if attempts ==0:
+        messagebox.showinfo("died","балбес")
+        root.quit()
+    elif all(letter in guessed_letters for letter in word):
+         messagebox.showinfo("чел хорош","но всё равно балбес")
+         root.quit() 
+
+    else:
+        word_label.config(text=display_word())      
 
 
 
+word_label=tk.Label(root,text=display_word(),font=30)
+word_label.pack()
+entry =tk.Entry(root,font=40)
+entry.pack(pady=10)
 
+entry.bind('<KeyRelease>',validate_count_symb)
+
+button = tk.Button(root,text="try it", font=25, command=check_letter)
+button.pack()
 
 
 
